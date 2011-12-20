c6ede0018b92e4e3a0c0ce172d229d0792a3b587
Vikiana Reyes <vikianareyes@gmail.com>
2011-12-20 13:12:32 -0500

    I comitted December20 still working in progress

---
+++ December20vreyes/src/ReminderEditActivity.java
@@ -0,0 +1,13 @@
+import android.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class ReminderEditActivity extends Activity {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate (savedInstanceState);
+		setContentView(R.layout.reminder_edit);
+	}
+
+}
---
+++ December20vreyes/src/com/android/december20vreyes/December20vreyesActivity.java
@@ -0,0 +1,31 @@
+package com.android.december20vreyes;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+public class December20vreyesActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.reminder_list);
+        
+        String [] items = new String [] { "Android Homework", "Drink After Finals", "Watch Allen Gregory Episode", "Xmas Shopping" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.reminder_row, R.id.text1, items);
+        setListAdapter(adapter);
+        
+        @Override
+        protected void onListItemClick (ListView 1, View v, int position, long id) {
+        	super.onListItemClick (1, v, position, id);
+        	
+        	registerForContextMenu(getListView());
+       
+        	@Override
+        	public void onCreateContextMenu (ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        		super.onCreateContextMenu(menu, v, menuInfo);
+        	}
+        }
+        
+    }
+}
